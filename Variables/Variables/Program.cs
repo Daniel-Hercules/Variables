1e732123502b3cc167facf782e917e267687d04f
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 17:11:54 +0000

    Add files via upload

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // One way of Variable declaration
+            string Name;
+
+            Name = "Daniel";
+
+            // Another way of Variable declaration
+
+            String Surname = "Hercules";
+
+            // Declaring another variable type (int)
+
+            int Age = 19;
+
+            Console.WriteLine("Hello " + Name + " " + Surname + " " + "you are : " + Age);
+        }
+    }
+}
